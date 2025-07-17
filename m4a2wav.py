1ae3ee0c2abd952369a4eb48a4a346a305b709b2
Oscar Reyes Hevia <oscar.reyes@usm.cl>
2025-07-16 21:56:01 -0400

    Mejora transcriber.py

--- m4a2wav.py
+++
@@ -1,6 +0,0 @@
-# Script para convertir M4A a WAV
-from pydub import AudioSegment
-
-# Convertir M4A a WAV
-audio = AudioSegment.from_file("test_audio.m4a", format="m4a")
-audio.export("test_audio.wav", format="wav")
